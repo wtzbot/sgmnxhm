4790661fae890e68f31ee16e6053b5917a31a658
Achiya CT <89849687+CTAchiya@users.noreply.github.com>
2022-01-03 14:21:40 +0530

    Update Ytdl.js

--- plugins/Ytdl.js
+++ plugins/Ytdl.js
@@ -44,13 +44,7 @@ Trex.addrex({pattern: 'play ?(.*)', fromMe: true, desc: Lang.PLAY_DESC}, (async 
         let arama = await yts(match[1]);
         arama = arama.all;
         if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-        var reply = await message.client.sendMessage(message.jid,`╭──────•◈•──────╮
-    sᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ꜱᴏɴɢ♪
-     ▄ ▅ ▆ ▇ █ █ ▇ ▆ ▅ ▄ 
-      ━━━━━ •♬• ━━━━━
- ╰──────•◈•──────╯
-﹏﹏﹏﹏﹏✪✭✪﹏﹏﹏﹏﹏
-   『T Rex Whatsapp Bot』`,MessageType.text);
+        var reply = await message.client.sendMessage(message.jid,`🔰 ➢ සින්දුව ඩව්න්ලෝඩ් වෙන ගමන් පොඩ්ඩක් ඉන්න 🤭....`,MessageType.text);
 
         let title = arama[0].title.replace(' ', '+');
         let stream = ytdl(arama[0].videoId, {
@@ -72,13 +66,7 @@ Trex.addrex({pattern: 'play ?(.*)', fromMe: true, desc: Lang.PLAY_DESC}, (async 
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,fs.readFileSync('./' + title + '.jpg'), MessageType.image, { caption: '\n```Song Name :\n'+ title +' ```\n\n'+`╭──────•◈•──────╮ 
-   ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ꜱᴏɴɢ♫
-       ılı.lıllılı.ıllı.ılı.lıllılı.ıllı.
-      ━━━━━ •♬• ━━━━━
- ╰──────•◈•──────╯
-﹏﹏﹏﹏﹏✪✭✪﹏﹏﹏﹏﹏
-   『T Rex Whatsapp Bot』`+'\n' });
+                reply = await message.client.sendMessage(message.jid,fs.readFileSync('./' + title + '.jpg'), MessageType.image, { caption: '\n```Song Name :\n'+ title +' ```\n\n'+`🔰 ➢ ඔයා ඉල්ලපු සින්දුව මන් අප්ලෝඩ් කරන ගමන් දැන් එයි 🤤...`+'\n' });
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg', contextInfo: { forwardingScore: 1, isForwarded: false }});
             });
     }));
