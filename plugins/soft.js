8b2553a1deca32a7b2a592891479e10894d8d85b
Achiya CT <89849687+CTAchiya@users.noreply.github.com>
2022-01-09 15:08:45 +0530

    Update soft.js

--- plugins/soft.js
+++ plugins/soft.js
@@ -11,20 +11,14 @@ if (Config.WORKTYPE == 'public') {
 
             var image = await axios.get ('https://telegra.ph/file/aeeaf4045664a272355be.png', {responseType: 'arraybuffer'})
 
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `
-╭───────────────
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
 │  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
 │
-│   SELECT YOUR SOFTWARE
-│
-│
-│
-│      coming soon 
-│
-│
-│
-│    
-│        
+│  adobe editing softwares type .adobe
+│  filmora softwares type       .fm
+│  operating system type        .os 
+│  avtivrus softwares type      .vs
+│  ms office softwares type     .msoffice   
 │
 │         🔰POWERD BY🔰
 │      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
@@ -33,12 +27,23 @@ if (Config.WORKTYPE == 'public') {
     }));
 
 
-    Trex.addrex({pattern: 'rhnx', fromMe: false,  deleteCommand: true }, (async (message, match) => {
+    Trex.addrex({pattern: 'sdgsjnerm', fromMe: false,  deleteCommand: true }, (async (message, match) => {
 
 
-            var image = await axios.get ('https://telegra.ph/file/873e00ec90306f5180f19.jpg', {responseType: 'arraybuffer'})
+            var image = await axios.get ('https://telegra.ph/file/a4b389af655b4dbb1df4f.png', {responseType: 'arraybuffer'})
 
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n🍁 RS.1000.00\n\n🍁 Dilivary available \n🍁Charge RS.300.00\n\n🍁 More details call 0775849690\n\n 👿 POWERD BY T REX 👿',quoted: message.data})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
+│  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
+│
+│  
+│  
+│  
+│  
+│   
+│
+│         🔰POWERD BY🔰
+│      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
+╰──────────────`,quoted: message.data})
 
 
 
@@ -47,8 +52,19 @@ if (Config.WORKTYPE == 'public') {
   Trex.addrex({pattern: 'sysymkasy', fromMe: false,  deleteCommand: true }, (async (message, match) => {
 
 
-            var image = await axios.get ('https://telegra.ph/file/60cd76df9eeab865aeb50.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n🍁 RS.1000.00\n\n🍁 Dilivary available \n🍁Charge RS.300.00\n\n🍁 More details call 0775849690\n\n 👿 POWERD BY T REX 👿',quoted: message.data})
+            var image = await axios.get ('https://telegra.ph/file/a4b389af655b4dbb1df4f.png', {responseType: 'arraybuffer'})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
+│  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
+│
+│  
+│  
+│  
+│  
+│   
+│
+│         🔰POWERD BY🔰
+│      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
+╰──────────────`,quoted: message.data})
 
   }));
 
@@ -56,7 +72,18 @@ if (Config.WORKTYPE == 'public') {
 
 
             var image = await axios.get ('https://telegra.ph/file/7639b67f052f3d4b34236.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n🍁 RS.1000.00\n\n🍁 Dilivary available \n🍁Charge RS.300.00\n\n🍁 More details call 0775849690\n\n 👿 POWERD BY T REX 👿',quoted: message.data})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
+│  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
+│
+│  
+│  
+│  
+│  
+│   
+│
+│         🔰POWERD BY🔰
+│      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
+╰──────────────`,quoted: message.data})
 
     }));
 
@@ -64,14 +91,36 @@ if (Config.WORKTYPE == 'public') {
 
 
             var image = await axios.get ('https://telegra.ph/file/db29720b4ac93686826c0.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n🍁 RS.1000.00\n\n🍁 Dilivary available \n🍁Charge RS.300.00\n\n🍁 More details call 0775849690\n\n 👿 POWERD BY T REX 👿',quoted: message.data})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
+│  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
+│
+│  
+│  
+│  
+│  
+│   
+│
+│         🔰POWERD BY🔰
+│      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
+╰──────────────`,quoted: message.data})
 
     }));
     Trex.addrex({pattern: 'xdyuzm,xu', fromMe: false,  deleteCommand: true }, (async (message, match) => {
 
 
             var image = await axios.get ('https://telegra.ph/file/2b5b8437d28720d6f2d79.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n🍁 RS.1000.00\n\n🍁 Dilivary available \n🍁Charge RS.300.00\n\n🍁 More details call 0775849690\n\n 👿 POWERD BY T REX 👿',quoted: message.data})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
+│  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
+│
+│  
+│  
+│  
+│  
+│   
+│
+│         🔰POWERD BY🔰
+│      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
+╰──────────────`,quoted: message.data})
 
     }));
 
@@ -81,7 +130,18 @@ if (Config.WORKTYPE == 'public') {
 
 
             var image = await axios.get ('https://telegra.ph/file/b260087f89c4f5cfd5bdd.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n🍁 RS.1000.00\n\n🍁 Dilivary available \n🍁Charge RS.300.00\n\n🍁 More details call 0775849690\n\n 👿 POWERD BY T REX 👿',quoted: message.data})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
+│  🔰 SOFTWARE STORE BY ACHI BRO LK 🔰
+│
+│  
+│  
+│  
+│  
+│   
+│
+│         🔰POWERD BY🔰
+│      ❰ 🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰 ❱
+╰──────────────`,quoted: message.data})
 
     }));
 
