ab8f52cb9721206a5a6e2d6c3cbf209e000f3945
wtzbot <96922881+wtzbot@users.noreply.github.com>
2022-01-23 06:50:55 -0800

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -82,4 +82,30 @@ else if (cn.WORKTYPE == 'public') {
     await reply.delete();
   })
     
+     Trex.addrex(
+  { pattern: "find", fromMe: true, desc: Lang.FIND_DESC },
+  async (message, match) => {
+    if (
+      !message.reply_message ||
+      (!message.reply_message.audio && !message.reply_message.video)
+    )
+      return await message.sendMessage(Lang.FIND_NEED_REPLY)
+    let location = await message.reply_message.downloadAndSaveMediaMessage(
+      "find"
+    )
+    let buff = await audioCut(location, 0, 15, "findo")
+    const data = await IdentifySong(buff)
+    if (!data) return
+    if (!data.status) return await message.sendMessage(Lang.NOT_FOUND)
+    return await message.sendMessage(
+      Lang.FIND_MSG.format(
+        data.data.title,
+        data.data.artists,
+        data.data.genre,
+        data.data.album,
+        data.data.release_date
+      ),
+      { quoted: message.quoted }
+    )
+    
 }
